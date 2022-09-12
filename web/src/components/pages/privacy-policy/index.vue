7e408253e4c28f199543cdbe9cf119d4b41f9a8f
captain-woof <captainwoof.official@gmail.com>
2022-09-12 15:35:57 +0530

    Privacy policy

--- web/src/components/pages/privacy-policy/index.vue
+++ web/src/components/pages/privacy-policy/index.vue
@@ -1,11 +1,83 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
-    
+    <h1 class="heading">Privacy Policy</h1>
+
+    <p class="sub-heading">Last updated August 13, 2022</p>
+
+    <p class="paragraph">
+        This privacy notice for Sohail Saha ("<b>Company</b>", "<b>we</b>", "<b>us</b>", or "<b>our</b>"), describes how
+        and why we might collect, store, use, and/or share ("process") your information when you use our services
+        ("Services"), such as when you:<br />
+
+    <ul class="list">
+        <li>
+            Download and use our mobile application (Note That), or any other application of ours that links to this
+            privacy notice.
+        </li>
+
+        <li>
+            Engage with us in other related ways, including any sales, marketing, or events.
+        </li>
+    </ul>
+    <br />
+    <b>Questions or concerns?</b> Reading this privacy notice will help you understand your privacy rights and choices.
+    If you do not agree with our policies and practices, please do not use our Services. If you still have any questions
+    or concerns, please contact us at captainwoof.official@gmail.com.
+    </p>
+
+    <h2 class="inner-heading">KEY POINTS</h2>
+
+    <p>This summary provides key points from our privacy notice. None of the points have been elaborated because we
+        don't collect any information, so nothing requires elaboration.</p>
+
+    <p><b>What personal information do we process?</b> None. We don't store any information. When (if) cloud storage
+        feature is released in the future, this privacy policy would be updated as well.</p>
+
+    <p><b>Do we process any sensitive personal information?</b> We do not process sensitive personal information.</p>
+
+    <p><b>Do we receive any information from third parties?</b> We do not receive any information from third parties.
+    </p>
+
+    <p><b>How do we process your information?</b> We don't collect any information. All information is stored on your
+        device only.</p>
+
+    <p><b>In what situations and with which parties do we share personal information?</b> Absolutely no such situation.
+    </p>
+
+    <p><b>How do we keep your information safe?</b> By not collecting it.</p>
+
+    <p><b>Do we make update to this notice?</b>As more and more (if) features get added, if it needs an update in the
+        policy, we'd update this notice and notify all users of the same.</p>
+
+    <p><b>How can you contact us about this notice?</b> On my (Sohail Saha) email: <a
+            href="mailto:captainwoof.official@gmail.com">captainwoof.official@gmail.com</a></p>
 </template>
 
 <style lang="css" scoped>
+.heading {
+    margin-top: var(--sp-700);
+}
 
+.sub-heading {
+    font-weight: 700;
+    text-decoration: underline;
+}
+
+.inner-heading {
+    margin-top: var(--sp-500);
+}
+
+.paragraph {}
+
+ul {
+    list-style: disc;
+}
+
+ol {
+    list-style: decimal;
+}
+
+a {
+    font-weight: 700;
+    text-decoration: underline;
+}
 </style>
